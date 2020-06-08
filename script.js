66aedec2edf2283ea7cee567ff908ac7e3daca2d
Tamas <hegtam90@gmail.com>
2020-06-08 20:12:14 +0200

    commit on master

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
 let slides = document.getElementsByClassName("slides");
+const displayed = document.getElementsByClassName("displayed");
+const slideShow = document.getElementById("slideshow");
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+let n = 0;
 
-console.log(slides)
+// console.log(slides.length);
+
+function displaySlide(n) {
+    let img = document.createElement('img');
+    img.setAttribute("src", slides[n].attributes.src.value);
+    img.classList.add("displayed");
+    slideShow.appendChild(img);
+}
+
+function deleteSlide() {
+    Array.from(displayed).forEach(element => {
+        element.remove();
+    });
+}
+
+
+
+function nextSlide() {
+    ++n;
+    if (n === slides.length) {
+        n = 0;
+        deleteSlide();
+        displaySlide(n);
+    } else {
+        deleteSlide();
+        displaySlide(n)
+    }
+}
+
+function prevSlide() {
+    --n;
+    console.log(n)
+    if (n < 0) {
+        n = slides.length - 1;
+        deleteSlide();
+        displaySlide(n);
+    } else {
+        deleteSlide();
+        displaySlide(n);
+    }
+}
+
+// displaySlide(n);
